a5e1e06e4ea3c26b4ef5816efe553d2bdbb5f217
Paolo Aliverti <paolo.aliverti@gmail.com>
2026-01-21 21:42:10 +0100

    Update WiFi password in IOT05_WebServer.ino

--- 2025/Siam-Arduino/L06-Iot/IOT05_WebServer/IOT05_WebServer.ino
+++ 2025/Siam-Arduino/L06-Iot/IOT05_WebServer/IOT05_WebServer.ino
@@ -9,7 +9,7 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  WiFi.begin("Siam-ReteAule", "Khimeya2018");
+  WiFi.begin("Siam-ReteAule", "xxxxxxx");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
